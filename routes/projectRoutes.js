01e4d790570b51ff3ceded703e90dba64a1f3f8f
G.azzu <gneiazmah@gmail.com>
2023-02-01 00:13:32 +1100

    Commit 1

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,47 +1,48 @@
 var express = require("express")
-var router = express.Router();
-//let client = require("../dbConnect");
-let projectCollection;
+var router = express.Router(); 
+let projectCollection; 
 let controller = require("../controller")
 
-// setTimeout(() =>{
-//     projectCollection = client.mongoClient.db().collection("Project")
-// },2000)
+// let client = require("../dbConnect"); 
+// setTimeout(() => {
+//     projectCollection = client.db().collection("Pets");
+// }, 2000)
 
+// //insert project....
 // const insertProjects = (project,callback) => {
 //     projectCollection.insert(project,callback);
 // }
 
+//post api....
 router.post('/',(req,res) => {
-    controller.createProjects(req,res)
+    controller.projectController.createProjects(req,res)
     // console.log("New Project added", req.body)
     // var newProject = req.body;
-
     // insertProjects(newProject,(err,result) => {
     //     if(err) {
     //         res.json({statusCode: 400, message: err})
     //     }
     //     else {
     //         res.json({statusCode: 200, message:"Project Successfully added", data: result})
-    //         }
+    //     }
     // })
 })
 
+// // get project...
 // const getProjects = (callback) => {
-//     projectCollection.find({}),toArray(callback);
+//     projectCollection.find({}).toArray(callback);
 // }
 
 router.get('/',(req,res) => {
-    controller.retrieveProjects(req,res)
-    // getProjects((err, result) => {
-    //     if(err){
+    controller.projectController.retrieveProjects(req,res)
+    // getProjects((err,result) => {
+    //     if(err) {
     //         res.json({statusCode: 400, message: err})
     //     }
-    //     else{
-    //         res.json({statusCode: 200, message: "Success", data: result})
+    //     else {
+    //         res.json({statusCode: 200, message:"Success", data: result})
     //     }
     // })
-
 })
 
-module.exports = router;
+module.exports = router; 
